67dbe07637e34ab40d8ecf779b8af775776541c3
stbnpng <jnestebn@protonmail.com>
2026-02-14 00:04:54 -0500

    src, and todo created, note: later try to make it it's own folder, there ain't many modules to try to make it all in the same spot

---
+++ src/todo.c
@@ -0,0 +1,7 @@
+/*
+* this program is a to do, terminal utility to set a fast to do list, mark and add new to-do entries, so i can have a more organized workflow 
+* copyright stbnpng 2026.
+*/
+
+#import <stdio.h>
+#import <stdint.h>
